43b64a400d1cabc94ec841ded5bdbe7294295ff7
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-12-05 01:40:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    @puppy = Puppy.new(parms)
+    erb :display_puppy
+  end
 
 end
